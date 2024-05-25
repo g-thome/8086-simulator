06af0bd51765432e87594332bfba4a3195bced24
Gabriel Thomé <gabrielallan.thome@gmail.com>
2024-05-25 14:31:26 -0300

    feat: get bytes in address

--- memory/memory.go
+++ memory/memory.go
@@ -13,6 +13,8 @@ type SegmentedAccess struct {
 	SegmentOffset uint32
 }
 
+const MEMORY_ACCESS_MASK = 0xfffff
+
 func LoadMemoryFromFile(fileName string, memory *Memory) int {
 	f, err := os.Open(fileName)
 	if err != nil {
@@ -26,4 +28,16 @@ func LoadMemoryFromFile(fileName string, memory *Memory) int {
 	}
 
 	return numberOfBytesRead
+}
+
+func GetAbsoluteAddressOf(segmentBase uint16, segmentOffset uint16, additionalOffset uint16) uint32 {
+	return ((uint32(segmentBase) << 4) + uint32(segmentOffset+additionalOffset)&MEMORY_ACCESS_MASK)
+}
+
+func ReadMemory(m *Memory, absoluteAddress uint32) byte {
+	if absoluteAddress > len(m.Bytes) {
+		panic("Tried accessing memory out of bounds")
+	}
+
+	return m.Bytes[absoluteAddress]
 }
