f451946bede59b2c5d763f8c8abbcc602699fb2b
Gabriel Thomé <gabrielallan.thome@gmail.com>
2024-06-09 13:26:30 -0300

    test: decode move accumulator to memory

--- decode/decode_test.go
+++ decode/decode_test.go
@@ -332,6 +332,60 @@ func TestDecodeMovAccMemory(t *testing.T) {
 	}
 }
 
+func TestDecodeMovAccToMemory(t *testing.T) {
+	at := memory.SegmentedAccess{0, 0}
+	ctx := DefaultDisAsmContext()
+	m := memory.Memory{}
+	memory.LoadMemoryFromFile("../fixtures/bin/mov_[2554]_ax", &m)
+
+	expected := instructions.Instruction{
+		Address: 0,
+		Size:    3,
+		Op:      instructions.OpMov,
+		Flags:   8,
+		Operands: [2]instructions.InstructionOperand{
+			{
+				Type: instructions.OPERAND_MEMORY,
+				Address: instructions.EffectiveAddressExpression{
+					Segment:      12,
+					Base:         instructions.EFFECTIVE_ADDRESS_DIRECT,
+					Displacement: 2554,
+				},
+				Register: registers.RegisterAccess{
+					Index:  registers.REGISTER_NONE,
+					Offset: 0,
+					Count:  0,
+				},
+				Immediate: instructions.Immediate{},
+			},
+			{
+				Type: instructions.OPERAND_REGISTER,
+				Address: instructions.EffectiveAddressExpression{
+					Segment:      0,
+					Base:         instructions.EFFECTIVE_ADDRESS_DIRECT,
+					Displacement: 0,
+				},
+				Register: registers.RegisterAccess{
+					Index:  registers.REGISTER_A,
+					Offset: 0,
+					Count:  2,
+				},
+				Immediate: instructions.Immediate{},
+			},
+		},
+	}
+
+	received, err := DecodeInstruction(&ctx, &m, &at)
+
+	if err != nil {
+		t.Fatalf(`Error decoding instruction %v`, err)
+	}
+
+	if !reflect.DeepEqual(received, expected) {
+		t.Fatalf("Expected \n%+v, \ngot \n%+v", expected, received)
+	}
+}
+
 func TestDecodeMovMemoryReg(t *testing.T) {
 	at := memory.SegmentedAccess{0, 0}
 	ctx := DefaultDisAsmContext()
